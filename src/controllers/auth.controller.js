f365dae4706e59df68aa434c10ebef31dadbef0f
Shashwata32 <mishrashashwata32@gmail.com>
2026-06-24 22:44:55 +0530

    auth established

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,67 @@
+const userModel = require('../models/user.model')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+async function registerUserController(req, res) {
+    const { username, email, password } = req.body
+
+    if(!username || !email || !password) {
+        return res.status(400).json({ message: 'Please provide username, email and password' })
+    }
+
+    const userAlreadyExists = await userModel.findOne({ $or: [{ username }, { email }] })
+
+    if(userAlreadyExists) {
+        return res.status(400).json({ message: 'Username or email already exists' })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+    res.cookie('token', token) // 1 day
+
+    res.status(201).json({ message: 'User registered successfully',
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+async function loginUserController(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    if(!user) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+    res.cookie('token', token) // 1 day
+
+    res.status(200).json({ message: 'User logged in successfully',
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+module.exports = { registerUserController, loginUserController }
