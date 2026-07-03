2e82c0aac689ac129eb48b01b61d50f1a53e6307
Shashwata32 <mishrashashwata32@gmail.com>
2026-07-04 00:21:18 +0530

    fix

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
     withCredentials: true
 })
 
